21f9ed3ab51e7decd5e047b67b57a0f177b7b4c8
carolbeck <caroolbeck1@gmail.com>
2026-03-17 19:22:44 -0300

    ex_array

--- js-aula-02-arrays-objetos-e-classes/ativ_array_objeto/script.js
+++ js-aula-02-arrays-objetos-e-classes/ativ_array_objeto/script.js
@@ -1,6 +1,6 @@
 let listaClientes = [
   {nome: "Anny Vitoria", endereco: "Rua Jose Alves, 123", cpf: "48287310923"},
-  {nome: "Natasha Pedroso",endereco: "Rua Antonio Golçalvess, 765", cpf:"37619835590"},
+  {nome: "Natasha Pedroso",endereco: "Rua Antonio Golçalves, 765", cpf:"37619835590"},
   {nome: "Caroline Beck",endereco: "Avenida Luiz Rangel,726",cpf: "50676609880"}
 ];
 
