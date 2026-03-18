1a7ac4faf5143941e9f0428f94367ce321a2a101
carolbeck <caroolbeck1@gmail.com>
2026-03-18 20:50:13 -0300

    oii

---
+++ nodejs-aula-03-2-parametros-em-rotas/index.js
@@ -0,0 +1,59 @@
+// O arquivo index.js é o arquivo principal do projeto
+
+// IMPORTANDO O MÓDULO DO "EXPRESS"
+const express = require("express");
+
+// CRIANDO UMA INSTÂNCIA DO EXPRESS
+const app = express();
+
+//CONFIGURANDO O EJS
+app.set("view engine", "ejs");
+
+// CRIANDO A ROTA PRINCIPAL DO SITE ("/")
+app.get("/", function(req, res){ //req- enviar um requisição para o servidor -- res- resposta para o usuario
+    // res.send("<h1>Bem-vindo ao meu primeiro site em Node.js e Express.js!</h1>");
+    res.render("index")
+});
+
+// CRIANDO A ROTA DE PERFIL DE USUARIO
+app.get("/perfil", function(req,res){
+    // res.send("<h2>Bem-vindo ao seu perfil!</h2>");
+    res.render("perfil")
+});
+app.get("/clientes", function(req,res){
+    res.render("clientes")
+})
+
+//rotas de produtos
+app.get("/produtos", function(req,res){
+    const produto = ""
+    res.render("produtos",{
+        produto: produto,
+    });
+});
+
+//rotas de produtos com parametro
+app.get("/produtos/:produto", function(req,res){
+    const produto = req.params.produto;
+    res.render("produtos",{
+        produto: produto,
+    });
+});
+
+
+app.get("/servicos", function(req,res){
+    res.render("servicos")
+})
+
+// INICIANDO O SERVIDOR NA PORTA 8080
+const port = 8080;
+app.listen(port, (error) => {
+    if (error){
+        console.log("Ocorreu um erro ao iniciar o servidor!" + error);
+    } else{
+        console.log(`Servidor iniciado com sucesso na porta no endereço: http://localhost:${port}`);
+    }
+});
+
+// node index.js 
+
